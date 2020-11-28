864f90a863a42629854662a65a78714d663c639d
Valentino Christian <valentinocc@ufl.edu>
2020-11-27 22:10:00 -0500

    added firebase database dependency and config setup

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 import { addCampaign } from './actions/campaigns';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
+import './firebase/firebase';
 
 const store = configureStore();
 
---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA_x2jZqt0E5ZRt9gAbXe1Z4Mf-kFz_l50",
+    authDomain: "deep-purple-725cc.firebaseapp.com",
+    databaseURL: "https://deep-purple-725cc.firebaseio.com",
+    projectId: "deep-purple-725cc",
+    storageBucket: "deep-purple-725cc.appspot.com",
+    messagingSenderId: "250624387003",
+    appId: "1:250624387003:web:6ac694b2f564ea9d74069e",
+    measurementId: "G-93PG8JNS04"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.database().ref().set({
+    name: 'Val Christian'
+});
